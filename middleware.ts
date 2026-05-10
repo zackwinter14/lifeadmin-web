b1103cd5c78e27bae8fe5e35f11fcc324c82a489
zackwinter14 <zackwinter14@gmail.com>
2026-05-09 22:46:37 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 // Routes that DON'T require login. Everything else is protected.
@@ -38,7 +38,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookies) {
+        setAll(cookies: { name: string; value: string; options: CookieOptions }[]) {
           cookies.forEach(({ name, value, options }) => {
             response.cookies.set(name, value, options);
           });
